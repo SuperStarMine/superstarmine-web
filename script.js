208c60e480557f3d736af03e62a5d62aa7f56766
HIBIKI CUBE <hibiki.apple.1984@gmail.com>
2020-01-29 15:43:51 +0900

    Dev 自動スクロールの挙動を調整

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 const card_container = document.getElementsByClassName("card-container");
-let scroll_offset = [0, 0];
-let auto_scroll = [true, true];
+const scroll_offset = [0, 0];
+const auto_scroll = [true, true];
+const distance = [1, 1]
+const time = [100, 100]
+let timer = [];
 
 for (let i = 0; i < card_container.length; i++) {
   card_container[i].onmouseover = () => auto_scroll[i] = false;
   card_container[i].ontouchstart = () => auto_scroll[i] = false;
 }
 
-if (auto_scroll) {
-  let timer = setInterval(() => {
-    for (let i = 0; i < card_container.length; i++) {
-      if (auto_scroll[i]) {
-        let before_scroll = card_container[i].scrollLeft;
-        scroll_offset[i] += 1
-        card_container[i].scrollTo({ left: scroll_offset[i], behavior: 'smooth' });
-        console.log(scroll_offset);
+for (let i = 0; i < card_container.length; i++) {
+  timer[i] = setInterval(() => {
+    if (auto_scroll[i]) {
+      let before_scroll = card_container[i].scrollLeft;
+      card_container[i].scrollTo({ left: scroll_offset[i]+=distance[i], behavior: 'smooth' });
+      if (card_container[i].scrollLeft == before_scroll) {
+        if (card_container[i].scrollLeft== 0) {
+          distance[i] = 1;
+          time[i] = 100;
+        } else {
+          distance[i] = -100;
+          time[i] = 1;
+        }
+        card_container[i].scrollTo({ left: scroll_offset[i]+=distance[i], behavior: 'smooth' });
       }
+    } else {
+      clearInterval(timer[i]);
     }
-  }, 100);
+  }, time[i]);
 }
