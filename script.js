62fffb597f1d96e9be6fcc8ede3178b7cff523ac
HIBIKI CUBE <hibiki.apple.1984@gmail.com>
2020-02-10 12:09:44 +0900

    Dev 自動スクロールの動作を改善

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-onload = () => {
+addEventListener("load", () => {
   const card_container = document.getElementsByClassName("card-container");
-  const main_section_title = document.getElementsByClassName("main-section_title");
-  const scroll_offset = [0, 0];
-  const auto_scroll = [true, true];
-  const distance = [1, 1]
-  const time = [80, 100]
+  const auto_scroll_resumer = document.getElementsByClassName("auto-scroll-resumer");
+  const scroll_offset = new Array(card_container.length).fill(0);
+  const auto_scroll = new Array(card_container.length).fill(true);
+  const distance = new Array(card_container.length).fill(1);
+  const time = new Array(card_container.length).fill(33);
   let timer = [];
 
   for (let i = 0; i < card_container.length; i++) {
@@ -13,39 +13,42 @@ onload = () => {
   }
 
   for (let i = 0; i < card_container.length; i++) {
-    main_section_title[i].onclick = () => {
-      auto_scroll[i] = true;
-      scroll_offset[i] = card_container[i].scrollLeft;
-    }
-    main_section_title[i].ontouchend = () => {
-      auto_scroll[i] = true;
-      scroll_offset[i] = card_container[i].scrollLeft;
-    }
-    auto_scroller();
+    auto_scroll_resumer[i].addEventListener("click", () => {
+      if (!auto_scroll[i]) {
+        auto_scroll[i] = true;
+        scroll_offset[i] = card_container[i].scrollLeft;
+      }
+    });
+    auto_scroll_resumer[i].addEventListener("touchstart", () => {
+      if (!auto_scroll[i]) {
+        auto_scroll[i] = true;
+        scroll_offset[i] = card_container[i].scrollLeft;
+      }
+    });
   }
 
   function auto_scroller() {
     for (let i = 0; i < card_container.length; i++) {
       timer[i] = setInterval(() => {
-        if (auto_scroll[i]) {
-          let before_scroll = card_container[i].scrollLeft;
+        if (auto_scroll[i] && card_container[i].scrollWidth > card_container[i].clientWidth) {
           card_container[i].scrollTo({ left: scroll_offset[i] += distance[i], behavior: 'smooth' });
-          if (card_container[i].scrollLeft == before_scroll) {
-            if (card_container[i].scrollLeft == 0) {
-              distance[i] = 1;
-              time[i] = 100;
-            } else {
-              distance[i] = -100;
-              time[i] = 1;
+          if (card_container[i].scrollLeft >= card_container[i].scrollWidth - card_container[i].clientWidth || card_container[0].scrollLeft <= 0) {
+            switch (card_container[i].scrollLeft) {
+              case 0:
+                distance[i] = 1;
+                time[i] = 33;
+                break;
+              default:
+                distance[i] = -50;
+                time[i] = 5;
+                break;
             }
             card_container[i].scrollTo({ left: scroll_offset[i] += distance[i], behavior: 'smooth' });
           }
-        } else {
-          clearInterval(timer[i]);
         }
       }, time[i]);
     }
   }
 
   auto_scroller();
-}
+});
