b8221f4bb1dd5fb8a080d52e6d7862fe696dd510
HIBIKI CUBE <hibiki.apple.1984@gmail.com>
2020-04-10 15:13:30 +0900

    Dev 変数名を明確化

--- script.js
+++ script.js
@@ -2,26 +2,26 @@ document.addEventListener("DOMContentLoaded", () => {
   const card_container = document.getElementsByClassName("card-container");
   const auto_scroll_resumer = document.getElementsByClassName("auto-scroll-resumer");
   const scroll_offset = new Array(card_container.length).fill(0);
-  const auto_scroll = new Array(card_container.length).fill(true);
+  const is_auto_scrolling = new Array(card_container.length).fill(true);
   const distance = new Array(card_container.length).fill(1);
   const time = new Array(card_container.length).fill(33);
   let timer = [];
 
   for (let i = 0; i < card_container.length; i++) {
-    card_container[i].addEventListener("mouseover", () => auto_scroll[i] = false);
-    card_container[i].addEventListener("touchend", () => auto_scroll[i] = false);
+    card_container[i].addEventListener("mouseover", () => is_auto_scrolling[i] = false);
+    card_container[i].addEventListener("touchend", () => is_auto_scrolling[i] = false);
   }
 
   for (let i = 0; i < card_container.length; i++) {
     auto_scroll_resumer[i].addEventListener("click", () => {
-      if (!auto_scroll[i]) {
-        auto_scroll[i] = true;
+      if (!is_auto_scrolling[i]) {
+        is_auto_scrolling[i] = true;
         scroll_offset[i] = card_container[i].scrollLeft;
       }
     });
     auto_scroll_resumer[i].addEventListener("touchstart", () => {
-      if (!auto_scroll[i]) {
-        auto_scroll[i] = true;
+      if (!is_auto_scrolling[i]) {
+        is_auto_scrolling[i] = true;
         scroll_offset[i] = card_container[i].scrollLeft;
       }
     });
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
   function auto_scroller() {
     for (let i = 0; i < card_container.length; i++) {
       timer[i] = setInterval(() => {
-        if (auto_scroll[i] && card_container[i].scrollWidth > card_container[i].clientWidth) {
+        if (is_auto_scrolling[i] && card_container[i].scrollWidth > card_container[i].clientWidth) {
           card_container[i].scrollTo({ left: scroll_offset[i] += distance[i], behavior: 'smooth' });
           if (card_container[i].scrollLeft >= card_container[i].scrollWidth - card_container[i].clientWidth || card_container[0].scrollLeft <= 0) {
             switch (card_container[i].scrollLeft) {
