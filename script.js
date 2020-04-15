bf5c935cccd9a10bd05447cfa29f6902bdc88686
HIBIKI CUBE <hibiki.apple.1984@gmail.com>
2020-04-15 14:36:22 +0900

    Dev requestAnimationFrameに対応 リファクタリング

--- script.js
+++ script.js
@@ -1,47 +1,52 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const card_container = document.getElementsByClassName("card-container");
-  const auto_scroll_resumer = document.getElementsByClassName("auto-scroll-resumer");
-  const scroll_offset = new Array(card_container.length).fill(0);
-  const is_auto_scrolling = new Array(card_container.length).fill(true);
-  const distance_value = 1;
-  const time_value = 33;
-  const distance = new Array(card_container.length).fill(distance_value);
-  const time = new Array(card_container.length).fill(time_value);
-  let timer = [];
+  const card_container = document.getElementsByClassName("card-container"),
+        auto_scroll_resumer = document.getElementsByClassName("auto-scroll-resumer"),
+        scroll_offset = new Array(card_container.length).fill(0),
+        is_paused = new Array(card_container.length).fill(false),
+        is_waiting = new Array(card_container.length).fill(false),
+        is_rewinding = new Array(card_container.length).fill(false),
+        speed_value = 45, //px/s
+        speed = new Array(card_container.length).fill(speed_value);
+  let last_time,
+      timeout = [];
 
   for (let i = 0; i < card_container.length; i++) {
-    card_container[i].addEventListener(window.ontouchstart ? "mouseover" : "touchend", () => is_auto_scrolling[i] = false, {passive: true});
+    card_container[i].addEventListener(window.ontouchstart ? "touchend" : "mouseover", () => is_paused[i] = true, { passive: true });
   }
 
   for (let i = 0; i < card_container.length; i++) {
-    auto_scroll_resumer[i].addEventListener(window.ontouchstart ? "touchstart" : "click", () => {
-      if (!is_auto_scrolling[i]) {
-        is_auto_scrolling[i] = true;
-        scroll_offset[i] = card_container[i].scrollLeft;
-      }
-    }, {passive: true});
+    auto_scroll_resumer[i].addEventListener(window.ontouchstart ? "touchstart" : "click", () => is_paused[i] = false, {passive: true});
   }
 
-  function auto_scroller() {
-    for (let i = 0; i < card_container.length; i++) {
-      timer[i] = setInterval(() => {
-        if (is_auto_scrolling[i] && card_container[i].scrollWidth > card_container[i].clientWidth) {
-          card_container[i].scrollTo({ left: scroll_offset[i] += distance[i], behavior: 'smooth' });
-          if (card_container[i].scrollLeft >= card_container[i].scrollWidth - card_container[i].clientWidth || card_container[i].scrollLeft <= 0) {
-            if (card_container[i].scrollLeft <= 0) {
-              card_container[i].scrollLeft = 0;
-              distance[i] = distance_value;
-              time[i] = time_value;
-            } else {
-              distance[i] = -50;
-              time[i] = 5;
-            }
-            card_container[i].scrollTo({ left: scroll_offset[i] += distance[i], behavior: 'smooth' });
-          }
-        }
-      }, time[i]);
+  function auto_scroller(time) {
+    if (!last_time) {
+      last_time = time;
+      console.log(`Start:${last_time}`);
+      requestAnimationFrame(auto_scroller);
+      return;
     }
+    for (let i = 0; i < card_container.length; i++) {
+      if (!is_waiting[i] && !is_paused[i] && card_container[i].scrollWidth > card_container[i].clientWidth) {
+        if (card_container[i].scrollLeft <= 0 && is_rewinding[i]) {
+          speed[i] = speed_value;
+          is_rewinding[i] = false;
+          is_waiting[i] = true;
+          timeout[i] = setTimeout(() => is_waiting[i] = false, 1000);
+        }
+        if (card_container[i].scrollLeft >= card_container[i].scrollWidth - card_container[i].clientWidth && !is_rewinding[i]) {
+          is_rewinding[i] = true;
+          is_waiting[i] = true;
+          timeout[i] = setTimeout(() => is_waiting[i] = false, 500);
+          speed[i] = -1000;
+        } else {
+          const d = (time - last_time) / 1e3 * speed[i];
+          card_container[i].scrollTo({ left: scroll_offset[i] += d, behavior: 'smooth' });
+        }
+      }
+    }
+    requestAnimationFrame(auto_scroller);
+    last_time = time;
   }
 
-  auto_scroller();
+  requestAnimationFrame(auto_scroller);
 }, {passive: true});
