f727aa2310e543b574c96c6bcc91bb114c0b4366
HIBIKI CUBE <hibiki.apple.1984@gmail.com>
2021-04-10 19:37:01 +0900

    Add cache-bustingに対応

---
+++ tools/cache-busting.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#This script works on BSD systems.
+
+d=$(pwd)
+cd $(git rev-parse --show-toplevel)
+
+for i in $(cat src/cache-busting_targets.txt)
+do
+  echo $i"\n"$(grep $i index.html|awk '{match($0, /h=[a-zA-Z0-9]{6}/);print substr($0, RSTART, RLENGTH)}')"\n> "$(sha256sum $i|awk '{print substr($1, 0, 6)}')"\n"
+  sed -i '' "s/$(grep $i index.html|awk '{match($0, /h=[a-zA-Z0-9]{6}/);print substr($0, RSTART, RLENGTH)}')/h=$(sha256sum $i|awk '{print substr($1, 0, 6)}')/g" index.html
+done
+
+cd $d
