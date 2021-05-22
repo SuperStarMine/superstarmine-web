5cbf9189a5f36786a539fb49e0226f18ead94031
HIBIKI CUBE <hibiki.apple.1984@gmail.com>
2021-05-23 01:37:15 +0900

    Dev ネクロマンスちゃんページの動画をVercelから読み込むように変更

--- src/pages/necromance.svelte
+++ src/pages/necromance.svelte
@@ -47,7 +47,7 @@
     })(document)
   });
 
-  const videoSourceUrl = '../../video/necromance_hero.m3u8';
+  const videoSourceUrl = 'https://dev.superstarmine.ga/video/necromance_hero.m3u8';
   const hls = new HLS({
     "startFragPrefetch": true
   });
