c580b895fa057de52c7ead6ff11e5c6116b9a144
kazuemon <git@k6n.jp>
2021-06-25 12:06:13 +0900

    add: eslint, prettier, stylelint を仮導入

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  plugins: [
+    'svelte3'
+  ],
+  overrides:[
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3'
+    }
+  ],
+  settings: {
+    'svelte3/ignore-styles': () => true
+  },
+  rules: {
+    'no-empty': 'off'
+  }
+}
